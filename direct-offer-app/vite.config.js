ea6b2204bc9374977ab6630797ca86de3eb58c29
Nathan Corley <corleynathan@gmail.com>
2026-02-18 23:44:52 -0600

    Fixing vite config import

--- direct-offer-app/vite.config.js
+++ direct-offer-app/vite.config.js
@@ -1,4 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  // base: './',  <-- If this line exists, try commenting it out!
 })
